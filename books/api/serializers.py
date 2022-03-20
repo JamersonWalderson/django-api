e459121bf136e20281a8544c57bf87f25e02256c
jamesw <36804142+JamersonWalderson@users.noreply.github.com>
2022-03-20 19:21:59 -0300

    chore: melhorando serialização

--- books/api/serializers.py
+++ books/api/serializers.py
@@ -1,4 +1,5 @@
 # https://labcodes.com.br/blog/pt-br/development/como-usar-serializers-de-django-rest-framework/
+#https://youtu.be/wtl8ZyCbTbg?t=787
 
 import rest_framework import serializers
 from Books import models
@@ -6,4 +7,14 @@ from Books import models
 class BooksSerializer(serializers.ModelSeriealizer):
   class Meta:
     model = models.Books
-    fields = '__ALL__'
+    fields = [
+      'id_book',
+      'title',
+      'author',
+      'release_year',
+      'state',
+      'pages',
+      'publish_company',
+      'create_at',
+    ]
+    # fields = '__ALL__'
