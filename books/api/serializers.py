dbbff2fc188d1d78bba4fc29ef9a1b8e23eabdbc
jamesw <36804142+JamersonWalderson@users.noreply.github.com>
2022-03-21 09:34:27 -0300

    bugfix: corrigindo erro do rest_framework

--- books/api/serializers.py
+++ books/api/serializers.py
@@ -1,7 +1,7 @@
 # https://labcodes.com.br/blog/pt-br/development/como-usar-serializers-de-django-rest-framework/
 #https://youtu.be/wtl8ZyCbTbg?t=787
 
-import rest_framework import serializers
+from rest_framework import serializers
 from Books import models
 
 class BooksSerializer(serializers.ModelSeriealizer):
@@ -17,4 +17,5 @@ class BooksSerializer(serializers.ModelSeriealizer):
       'publish_company',
       'create_at',
     ]
+    querySet = models.Books.objects.all()
     # fields = '__ALL__'
