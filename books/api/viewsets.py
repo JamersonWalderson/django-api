b3a2d5dfc2e9737840bab67b3612da85af4f390b
jamesw <36804142+JamersonWalderson@users.noreply.github.com>
2022-03-22 15:12:36 -0300

    implementation: adicionando mensagem personalizada na criação

--- books/api/viewsets.py
+++ books/api/viewsets.py
@@ -1,3 +1,4 @@
+from rest_framework.response import Response
 from rest_framework import viewsets
 from books.api import serializers
 from books import models
@@ -5,3 +6,7 @@ from books import models
 class BooksViewSet(viewsets.ModelViewSet):
   serializer_class = serializers.BooksSerializer
   queryset = models.Books.objects.all()
+
+
+  def create(self, request):
+    return Response( {'message': 'Livro cadastrado com sucesso.'})
