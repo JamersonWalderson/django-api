eae976632f5a2bb217c834390d97c0ab3e392318
jamesw <36804142+JamersonWalderson@users.noreply.github.com>
2022-03-22 15:57:21 -0300

    implementation: adicionando 'status' e 'message' ao viewset create

--- books/api/viewsets.py
+++ books/api/viewsets.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework import viewsets
+from rest_framework import status
 from books.api import serializers
 from books import models
 
@@ -7,6 +8,20 @@ class BooksViewSet(viewsets.ModelViewSet):
   serializer_class = serializers.BooksSerializer
   queryset = models.Books.objects.all()
 
-
   def create(self, request):
-    return Response( {'message': 'Livro cadastrado com sucesso.'})
+    serializer = self.get_serializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(
+        {
+          'status': 'success',
+          'message': 'Livro cadastrado com sucesso',
+          'data': serializer.data
+        }, status=status.HTTP_201_CREATED)
+    else:
+      return Response(
+        {
+          'status': 'error',
+          'message' 'Erro ao cadastrar'
+          'data': serializer.error
+          }, status=status.HTTP_400_BAD_REQUEST)
