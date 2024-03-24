c266f9d55b5335c1eec3c7d8b551479837a955d5
JamersonWalderson <jamersonwalderson@gmail.com>
2024-03-24 10:27:37 -0300

    doc: removendo comentários desnecessários

--- frontend/pages/api/auth/[...nextauth].ts
+++ frontend/pages/api/auth/[...nextauth].ts
@@ -40,12 +40,7 @@ export const authOptions: AuthOptions = {
   // https://next-auth.js.org/configuration/providers/oauth
   providers: [
     CredentialsProvider({
-      // The name to display on the sign in form (e.g. 'Sign in with...')
       name: "Django Rest Framework",
-      // The credentials is used to generate a suitable form on the sign in page.
-      // You can specify whatever fields you are expecting to be submitted.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
         username: {
           label: "Username",
